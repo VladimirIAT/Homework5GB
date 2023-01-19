26cb857ad1816534c8372105de4f0fe0f2971e39
Vladimir <volodko127@mail.ru>
2023-01-19 23:25:04 +0300

    Solution

---
+++ Task34/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу реализующую методы формирования массива, заполненного случайными положительными 
+//трёхзначными числами, и подсчета количества чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+using static System.Console;
+Clear();
+
+int[] array = GetArray(10);
+PrintArray(array);
+int count = countNumber(array);
+Write(count);
+
+int[] GetArray(int size)
+{
+    int[] resultArray = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        resultArray[i] = rnd.Next(100, 999);
+    }
+    return resultArray;
+}
+
+int countNumber(int[] inArray)
+{
+    int result = 0;
+    foreach (int i in inArray)
+    {
+        if (i % 2 == 0) result++;
+    }
+    return result;
+}
+
+void PrintArray(int[] inArray)
+{
+    Write("[");
+        for (int i = 0; i < inArray.Length - 1; i++)
+            {
+                Write($"{inArray[i]},");
+            }
+    Write($"{inArray[inArray.Length - 1]}]");
+}
---
+++ Task38/Program.cs
@@ -0,0 +1,50 @@
+﻿// Напишите программу реализующую методы формирования массива, заполненного случайными вещественными числами и 
+//вычисления разницы между максимальным и минимальным элементов массива.
+//[3,3 7,1 22,5 2,2 78,2] -> 76
+using static System.Console;
+Clear();
+
+int[] array = GetArray(10);
+PrintArray(array);
+int count = findMinMax(array);
+Write(count);
+
+int[] GetArray(int size)
+{
+    int[] resultArray = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        resultArray[i] = rnd.Next(0, 10);
+    }
+    return resultArray;
+}
+
+int findMinMax(int[] inArray)
+{
+    int minNum = inArray[0];
+    int maxNum = inArray[0];
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        if (inArray[i] < minNum) 
+        {
+            minNum = inArray[i + 1];
+        }
+        if (inArray[i] > maxNum) 
+        {
+            maxNum = inArray[i];
+        }
+    }
+    int result = maxNum - minNum;
+    return result;
+}
+
+void PrintArray(int[] inArray)
+{
+    Write("[");
+        for (int i = 0; i < inArray.Length - 1; i++)
+            {
+                Write($"{inArray[i]},");
+            }
+    Write($"{inArray[inArray.Length - 1]}]");
+}
